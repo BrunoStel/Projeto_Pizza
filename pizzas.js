84516f3fb251452f6e921a434ef5fd71af1cd20f
Bruno Stelmastchuk <brunostel@gmail.com>
2021-10-29 16:37:12 -0300

    Adicionando funcionalidades

--- pizzas.js
+++ pizzas.js
@@ -1,9 +1,9 @@
 let pizzaJson = [
-    {id:1, name:'Mussarela', img:'images/pizza.png', price:20.19, sizes:['100g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:2, name:'Calabresa', img:'images/pizza2.png', price:18.00, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:17.45, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:4, name:'Americana', img:'images/pizza4.png', price:19.77, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:5, name:'Sorvete', img:'images/pizza5.png', price:21.43, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:1, name:'Mussarela', img:'images/pizza.png', price:20.19, sizes:['120g', '400g', '600g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:2, name:'Calabresa', img:'images/pizza2.png', price:18.00, sizes:['250g', '550g', '720g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:17.45, sizes:['320g', '530g', '780g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:4, name:'Americana', img:'images/pizza4.png', price:19.77, sizes:['300g', '450g', '800g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:5, name:'Sorvete', img:'images/pizza5.png', price:21.43, sizes:['220g', '430g', '700g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
     {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:18.55, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
-    {id:7, name:'Chocolate', img:'images/pizza7.png', price:22.36, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'}
+    {id:7, name:'Chocolate', img:'images/pizza7.png', price:22.36, sizes:['280g', '400g', '800g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'}
 ];
--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 
 
 //Criando a função de construção do HTML com os dados da API(que no caso é um objeto, simulando uma API)
-let response = (id,imagem, preco, descricao, nome) =>{
+let response = (id,imagem, preco, descricao, nome,sizes) =>{
   //Definindo o evento onclick na tag <a>, sendo passado como argumento os values de cada objeto
+ 
   document.querySelector('.pizza-area').innerHTML+= 
             `<div class='pizza-item'>
-                <a href='#' onclick="monstrarWindowPizza('${imagem}','${descricao}', '${nome}',${preco})">
+                <a href='#' onclick="monstrarWindowPizza('${imagem}','${descricao}', '${nome}',${preco},'${sizes}')">
                     <img class='pizza-item--img' src="${imagem}" />
                     <div class='pizza-item--add'>+</div>
                 </a>
@@ -13,21 +14,38 @@ let response = (id,imagem, preco, descricao, nome) =>{
                 <div class='pizza-item--price'>R$ ${preco}</div>
                 <div class='pizza-item--desc'>${descricao}</div>
             </div>`
+    
 } 
 
 //POuplando o index com as pizzas
+
 pizzaJson.forEach((obj)=>{
-    response(obj.id, obj.img, obj.price, obj.description, obj.name)
+   
+    response(obj.id, obj.img, obj.price, obj.description, obj.name,obj.sizes)
 })
 
 //Pegando os values passados como argumento na construção do HTML, para exibi-los quando clicar na pizza
-const monstrarWindowPizza = (imagem, descricao, nome, preco) => {
+const monstrarWindowPizza = (imagem, descricao, nome, preco, sizes) => {
+    let arrTamanhos = sizes.split(',')
     document.querySelector('.pizzaWindowArea').style.display = 'flex'
     document.querySelector('.imagem-window').setAttribute('src', imagem)
     document.querySelector('.pizzaInfo--desc').innerHTML = descricao
     document.querySelector('.pizza_name').innerHTML = nome
     document.querySelector('.pizzaInfo--actualPrice').innerHTML = preco
+
+    document.querySelectorAll('.pizzaInfo--size').forEach(elem=>{
+        switch(parseInt(elem.dataset.key)){
+            case 0 : document.querySelectorAll('.pizzaInfo--size')[0].innerHTML = arrTamanhos[0]
+                break
+            case 1 : document.querySelectorAll('.pizzaInfo--size')[1].innerHTML = arrTamanhos[1]
+                break
+            case 2 : document.querySelectorAll('.pizzaInfo--size')[2].innerHTML = arrTamanhos[2]
+                break 
+        }})
+  
 }
+
+
 
 //Função para fechar a window
 const fecharWindow = () => {
